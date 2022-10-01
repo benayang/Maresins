596230e10a6636963be30511cd4e1c34821b28b4
NOBEL <benyang@umich.edu>
2022-09-30 22:04:34 -0400

    put code in functions

--- run_dpgp.sh
+++ run_dpgp.sh
@@ -2,11 +2,11 @@
 # conda activate py27
 
 DP_GP_cluster.py \
--i "/nas/homes/benyang/Maresins/Tables/all_fc_zscore.tsv" \
+-i "/nas/homes/benyang/Maresins/Tables/all_data_fc_zscore.tsv" \
 --plot --plot_types pdf \
 --output "/nas/homes/benyang/Maresins/DPGP_within_timepoint/all_within_timepoint"
 
-# DP_GP_cluster.py \
-# -i "/nas/homes/benyang/Maresins/Tables/sig_fc_zscore.tsv" \
-# --plot --plot_types pdf \
-# --output "/nas/homes/benyang/Maresins/DPGP_within_timepoint/sig_within_timepoint"
+DP_GP_cluster.py \
+-i "/nas/homes/benyang/Maresins/Tables/sig_data_fc_zscore.tsv" \
+--plot --plot_types pdf \
+--output "/nas/homes/benyang/Maresins/DPGP_within_timepoint/sig_within_timepoint"
